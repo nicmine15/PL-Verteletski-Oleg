80c97787fa9433c0d7b0a850d194be0efe0754df
Nic Mine <95490761+nicmine15@users.noreply.github.com>
2024-10-02 16:37:12 +0300

    6.py

--- 6.py
+++ 6.py
@@ -2,7 +2,7 @@ a = int(input())
 a1 = int(input())
 b = int(input())
 b1 = int(input())
-if a <= 8 and a1 <= 8 and b <= 8 and b1 <= 8:
+if (a <= 8 and a1 <= 8 and b <= 8 and b1 <= 8) and (a > 0 and a1 > 0 and b > 0 and b1 > 0):
     if (a % 2 == 0 and a1 % 2 == 0 and b % 2 == 0 and b1 % 2 == 0) or \
         (a % 2 == 0 and a1 % 2 == 0 and b % 2 != 0 and b1 % 2 != 0) or \
         (a % 2 != 0 and a1 % 2 != 0 and b % 2 == 0 and b1 % 2 == 0) or \
@@ -14,3 +14,5 @@ if a <= 8 and a1 <= 8 and b <= 8 and b1 <= 8:
         print("Да")
     else:
         print("Нет")
+else:
+    print('Цифры вне доски')
